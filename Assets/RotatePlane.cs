ef2406fb3f1442b9aec788ee5c15566d23186719
naojitaniguchi <naoji.taniguchi@gmail.com>
2017-09-20 10:38:13 +0900

    first commit .

---
+++ Assets/RotatePlane.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlane : MonoBehaviour {
+    public GameObject CameraObject;
+    float radius;
+    float init_y;
+    public float angleSpeed ;
+    public float startAngle ;
+    float angle;
+
+	// Use this for initialization
+	void Start () {
+        Vector3 pos;
+
+        pos.x = transform.position.x;
+        pos.y = 0.0f;
+        pos.z = transform.position.z;
+        init_y = transform.position.y;
+
+        radius = pos.magnitude;
+
+        angle = startAngle;
+        float x = Mathf.Cos(angle);
+        float z = Mathf.Sin(angle);
+
+        transform.position = new Vector3(x * radius, init_y, z * radius);
+
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        angle += angleSpeed * Time.deltaTime;
+        float x = Mathf.Cos(angle);
+        float z = Mathf.Sin(angle);
+        transform.position = new Vector3(x * radius, init_y, z * radius);
+
+        // Debug.Log(angle);
+        transform.LookAt(CameraObject.transform,Vector3.up);
+    }
+}
