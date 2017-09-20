733a6dfa84731df5f9974db4a938a7ed0696ba2d
naojitaniguchi <naoji.taniguchi@gmail.com>
2017-09-20 11:56:14 +0900

    added ChangePlane .

---
+++ Assets/ChangePlane.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePlane : MonoBehaviour {
+    public GameObject[] planes;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            dispPlane(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            dispPlane(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            dispPlane(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            dispPlane(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            dispPlane(4);
+        }
+
+    }
+
+    public void dispPlane(int index)
+    {
+        for ( int i = 0; i < planes.Length; i ++)
+        {
+            if ( i == index)
+            {
+                planes[i].SetActive(true);
+            }
+            else
+            {
+                planes[i].SetActive(false);
+            }
+        }
+    }
+}
